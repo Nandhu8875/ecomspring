5b5c5d9822d03ea069d1293b8e8daa02368a6567
Nandhu8875 <164969613+Nandhu8875@users.noreply.github.com>
2025-12-16 03:12:57 +0530

    Add files via upload

---
+++ srn_spring/demo/demo/src/main/java/com/srn/demo/SrnApplication.java
@@ -0,0 +1,13 @@
+package com.srn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrnApplication.class, args);
+	}
+
+}
---
+++ srn_spring/demo/demo/src/test/java/com/srn/demo/SrnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
